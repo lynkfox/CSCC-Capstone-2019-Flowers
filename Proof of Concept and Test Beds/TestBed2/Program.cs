b19b2a5e81b0a78d58704f8ab5727b9b7b5b3ba2
Lynkfox <anthony.t.goh@gmail.com>
2019-06-19 21:44:20 -0400

    Added TestBed2
    Testing the CCnSession Class (after this renamed to CcnSession.SQL) -  consolidating functions and adding Salted Hash to the pw creation and checks.

---
+++ Proof of Concept and Test Beds/TestBed2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TestBed2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname, lname, pw, username;
+
+
+            /*
+            Console.WriteLine("First Name: ");
+            fname = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            lname = Console.ReadLine();
+            Console.WriteLine("Desired PW: ");
+            pw = Console.ReadLine();
+
+            username = CcnSession.CcnSession.CreateUser(fname, lname, pw);
+
+            if (username != null)
+                Console.WriteLine("Success: Username is " + username);
+            else
+                Console.WriteLine("Something went wrong)");
+            */
+
+            
+
+            Console.WriteLine("Username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("PW: ");
+            pw = Console.ReadLine();
+
+            CcnSession.CcnSession.Setup(username, pw);
+
+            if (CcnSession.CcnSession.PwCorrect)
+                Console.WriteLine("Success. Correct PW");
+            else
+                Console.WriteLine("Failure. Wrong PW");
+
+            Console.ReadLine();
+            
+
+
+
+
+
+            
+        }
+    }
+}
